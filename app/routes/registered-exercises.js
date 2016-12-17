61327f1388e648b370d4fd6269c9da49e6b76f71
Federico Kauffman <federico@wyeworks.com>
2016-12-17 15:03:58 -0300

    Move templates

--- app/routes/registered-exercises.js
+++ app/routes/registered-exercises.js
@@ -2,19 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    let registers = this.store.findAll('register');
-    let exercises = this.store.findAll('exercise');
-
-    return Ember.RSVP.hash({
-      registers,
-      exercises
-    });
-  },
-
-  actions: {
-    registerExercise(exercise) {
-      let register = this.store.createRecord('register', { exercise });
-      this.transitionTo('exercises.register', register);
-    }
+    return this.store.findAll('registered-exercise');
   }
 });
