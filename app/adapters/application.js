e5f3e3936dce775d610993793cea74fc0ab60efb
Federico Kauffman <federico@wyeworks.com>
2016-12-18 13:36:07 -0300

    Add user paths

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,4 +1,19 @@
+import Ember from 'ember';
 import FirebaseAdapter from 'emberfire/adapters/firebase';
 
 export default FirebaseAdapter.extend({
+  session: Ember.inject.service(),
+
+  pathForType(type) {
+    let session = this.get('session');
+
+    if (session.get('isAuthenticated') && type !== 'exercise') {
+      let uid = this.get('session.currentUser.uid');
+      let path = Ember.String.pluralize(type);
+
+      return  `users/${uid}/${path}`;
+    } else {
+      return this._super(type);
+    }
+  }
 });
