18af3dc15562285702bbd206ad0710a1d963b341
Federico Kauffman <federico@wyeworks.com>
2016-12-19 19:19:14 -0300

    Add default value

--- app/models/exercise.js
+++ app/models/exercise.js
@@ -2,5 +2,13 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr(),
-  type: DS.attr() // in ['weightlifting', 'cardio']
+  type: DS.attr(), // in ['weightlifting', 'cardio']
+
+  isCardio() {
+    return this.get('type') === 'cardio';
+  },
+
+  isWeightlifting() {
+    return this.get('type') === 'weightlifting';
+  }
 });
--- app/models/register.js
+++ app/models/register.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  weight: DS.attr('number'),
+  weight: DS.attr('number', { defaultValue: null }),
+  time: DS.attr({ defaultValue: null }),
   date: DS.attr('date'),
 
   shortDate: Ember.computed('date', function() {
