be9bb6165a9075a7ddae9bd6d781ce437e39a823
Federico Kauffman <federico@wyeworks.com>
2016-12-19 19:08:42 -0300

    Show heart for cardio

--- app/components/registered-exercise-info.js
+++ app/components/registered-exercise-info.js
@@ -3,6 +3,11 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   showMoreInfo: false,
 
+  exerciseTypeClass: Ember.computed('model.exercise.type', function() {
+    let type = this.get('model.exercise.type');
+    return `card-media-${type}`;
+  }),
+
   actions: {
     moreInfo() {
       this.toggleProperty('showMoreInfo');
